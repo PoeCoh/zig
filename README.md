b87f39817d17885b2a93c17caaac98eef58a2186
Ben Jordan <ben@poecoh.com>
2024-05-18 16:38:59 -0400

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a simple example of how to import and use a module from another repository for [this page](ben.peocoh.com/)
+
---
+++ src/main.zig
@@ -0,0 +1,7 @@
+const std = @import("std");
+const remote_module = @import("remote_module");
+
+pub fn main() void {
+    const result = remote_module.hello();
+    std.debug.print("Hello {}\n", .{result});
+}
