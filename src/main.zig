838f7f821fbe97100a6844e6b139ae556c50bbfe
Ben Jordan <ben@poecoh.com>
2024-05-18 16:46:10 -0400

    forgot s.

--- src/main.zig
+++ src/main.zig
@@ -3,5 +3,5 @@ const remote_module = @import("remote_module");
 
 pub fn main() void {
     const result = remote_module.hello();
-    std.debug.print("Hello {}\n", .{result});
+    std.debug.print("Hello {s}\n", .{result});
 }
